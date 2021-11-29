6b95b813a39da581bae1597ee2105f68ba0142ab
Kimberly Reddick <kim.reddick@icloud.com>
2021-11-29 13:03:43 -0600

    Added feature to filter recipes.

--- app/recipe/tests/tests_ingredients_api.py
+++ app/recipe/tests/tests_ingredients_api.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 
-from core.models import Ingredient
+from core.models import Ingredient, Recipe
 
 from recipe.serializers import IngredientSerializer
 
@@ -79,3 +79,50 @@ class PrivateIngredientsApiTests(TestCase):
         payload = {'name': ' '}
         resp = self.client.post(INGREDIENTS_URL, payload)
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_ingredients_assigned_to_recipes(self):
+        # Test filtering only ingredients assigned to recipes.
+        ingredient1 = Ingredient.objects.create(
+            user=self.user,
+            name='Apples'
+        )
+        ingredient2 = Ingredient.objects.create(
+            user=self.user,
+            name='Peaches'
+        )
+        recipe1 = Recipe.objects.create(
+            title='Apple Pancakes',
+            time_minutes=5,
+            price=5.00,
+            user=self.user
+        )
+        recipe1.ingredients.add(ingredient1)
+        resp = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+
+        serializer1 = IngredientSerializer(ingredient1)
+        serializer2 = IngredientSerializer(ingredient2)
+        self.assertIn(serializer1.data, resp.data)
+        self.assertNotIn(serializer2.data, resp.data)
+
+    def test_retrieve_ingredients_assigned_unique(self):
+        # Test filtering unique ingredient list.
+        ingredient1 = Ingredient.objects.create(
+            user=self.user,
+            name='Apples'
+        )
+        recipe1 = Recipe.objects.create(
+            title='Apple Pancakes',
+            time_minutes=5,
+            price=5.00,
+            user=self.user
+        )
+        recipe1.ingredients.add(ingredient1)
+        recipe2 = Recipe.objects.create(
+            title='Apple Crumble',
+            time_minutes=10,
+            price=8.00,
+            user=self.user
+        )
+        recipe2.ingredients.add(ingredient1)
+        resp = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+        self.assertEqual(len(resp.data), 1)
